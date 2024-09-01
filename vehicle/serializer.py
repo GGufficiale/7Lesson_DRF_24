19ab176e05cab7f118272a828787788aa7c8d3ea
GGufficiale <germangranovskii@mail.ru>
2024-09-01 20:32:45 +0300

    Добавлена модель пробега для всех ТС.

--- vehicle/serializer.py
+++ vehicle/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from vehicle.models import Car, Moto
+from vehicle.models import Car, Moto, Milage
 
 
 class CarSerializer(serializers.ModelSerializer):
@@ -11,4 +11,10 @@ class CarSerializer(serializers.ModelSerializer):
 class MotoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Moto
+        fields = '__all__'
+
+
+class MilageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Milage
         fields = '__all__'
